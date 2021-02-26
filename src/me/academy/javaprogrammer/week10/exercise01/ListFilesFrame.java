12783699820c0d5a510e9e214237c74e38a629af
Aemilus <35335174+Aemilus@users.noreply.github.com>
2021-02-26 11:33:15 +0200

    week10 update

--- src/me/academy/javaprogrammer/week10/exercise01/ListFilesFrame.java
+++ src/me/academy/javaprogrammer/week10/exercise01/ListFilesFrame.java
@@ -3,30 +3,32 @@ package me.academy.javaprogrammer.week10.exercise01;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public final class ListFilesFrame extends JFrame {
+    // inputPanel and buttonsPanel will be placed on below rootPanel
     private final JPanel rootPanel = new JPanel();
 
+    // place all the components for gathering user input on inputPanel
     private final JPanel inputPanel = new JPanel();
     private final JPanel fileExtensionPanel = new JPanel();
     private final JTextField fileExtensionTextField = new JTextField();
-    private final JPanel directoryPanel = new JPanel();
-    private final JTextField directoryTextField = new JTextField();
+    private final JPanel chosenDirectoryPanel = new JPanel();
+    private final JTextField chosenDirectoryTextField = new JTextField();
+    private File selectedDirectory;
 
+    // place all JButtons on below panel
+    // on press of browseButton the directoryChooser open dialog will be shown
     private final JPanel buttonsPanel = new JPanel();
     private final JButton browseButton = new JButton("Browse");
-    private final JButton listButton = new JButton("List files");
+    private final JFileChooser directoryChooser = new JFileChooser();
+    private final JButton listFilesButton = new JButton("List files");
 
-//    private final JPanel listPanel = new JPanel();
+    // a JScrollPane to display the JList of files matching the extension on listFilesButton press
     private final JScrollPane listPanel = new JScrollPane();
     private final DefaultListModel<Path> pathsListModel = new DefaultListModel<>();
     private final JList<Path> pathsList = new JList<>(pathsListModel);
-
-    private final JFileChooser choseDirectory = new JFileChooser();
-    private File selectedDirectory;
 
     public ListFilesFrame() {
         super("List Files");
@@ -59,13 +61,13 @@ public final class ListFilesFrame extends JFrame {
 
     private void initInputPanel() {
         initFileExtensionPanel();
-        initDirectoryPanel();
+        initChosenDirectoryPanel();
         initButtonsPanel();
 
         inputPanel.setBorder(BorderFactory.createTitledBorder("Settings"));
         inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
         inputPanel.add(fileExtensionPanel);
-        inputPanel.add(directoryPanel);
+        inputPanel.add(chosenDirectoryPanel);
         inputPanel.add(buttonsPanel);
     }
 
@@ -75,36 +77,36 @@ public final class ListFilesFrame extends JFrame {
         fileExtensionPanel.add(fileExtensionTextField, BorderLayout.CENTER);
     }
 
-    private void initDirectoryPanel() {
-        directoryPanel.setBorder(BorderFactory.createTitledBorder("Directory Path"));
-        directoryPanel.setLayout(new BorderLayout());
-        directoryPanel.add(directoryTextField, BorderLayout.CENTER);
+    private void initChosenDirectoryPanel() {
+        chosenDirectoryPanel.setBorder(BorderFactory.createTitledBorder("Directory Path"));
+        chosenDirectoryPanel.setLayout(new BorderLayout());
+        chosenDirectoryPanel.add(chosenDirectoryTextField, BorderLayout.CENTER);
     }
 
     private void initButtonsPanel() {
         buttonsPanel.setLayout(new GridLayout(1, 2));
         buttonsPanel.add(browseButton);
-        buttonsPanel.add(listButton);
+        buttonsPanel.add(listFilesButton);
     }
 
     private void initListPanel() {
-//        listPanel.setLayout(new BorderLayout());
         listPanel.setBorder(BorderFactory.createTitledBorder("List of files"));
         listPanel.setViewportView(pathsList);
     }
 
     private void initListeners() {
+        directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         browseButton.addActionListener(e -> {
-            choseDirectory.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int option = choseDirectory.showOpenDialog(this);
+            int option = directoryChooser.showOpenDialog(this);
             if (option == JFileChooser.APPROVE_OPTION) {
-                selectedDirectory = choseDirectory.getSelectedFile();
-                directoryTextField.setText(selectedDirectory.getAbsolutePath());
+                selectedDirectory = directoryChooser.getSelectedFile();
+                chosenDirectoryTextField.setText(selectedDirectory.getAbsolutePath());
             }
         });
 
-        listButton.addActionListener(e -> {
-            String globbing = "*." + fileExtensionTextField.getText();
+        listFilesButton.addActionListener(e -> {
+            if (selectedDirectory == null) return;
+            String globbing = "*." + fileExtensionTextField.getText().trim();
             Path path = selectedDirectory.toPath();
             List<Path> list = ListFiles.getListOfPaths(this, path, globbing);
             pathsListModel.clear();
--- src/me/academy/javaprogrammer/week10/exercise01/readme.md
+++ src/me/academy/javaprogrammer/week10/exercise01/readme.md
@@ -1,4 +1,12 @@
 Exercise 01
 
 - List all files with a given extension in a given directory.
-For example: list all JPG files in users home directory.
+For example: list all JPG files in users Picture directory.
+  
+Result of current implementation:
+
+![List Files screenshot](list_files_1.png)
+
+![List Files screenshot](list_files_browse.png)
+
+![List Files screenshot](list_files_2.png)
